51f5bb5dd5d3143b89f634b5e4bc1a9c4e8a7779
Greg Sexton <gregsexton@gmail.com>
2012-11-12 22:46:13 +0000

    Ex 1.{1,2,3}

--- src/sicp/ch1.clj
+++ src/sicp/ch1.clj
@@ -1,2 +1,15 @@
 (ns sicp.ch1)
 
+;;; Exercise 1.1
+
+;;; Exercise 1.2
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))               ; -37/150
+
+;;; Exercise 1.3
+
+(defn sum-square-larger [a b c]
+  (defn square [n] (* n n))
+  (- (apply + (map square (vector a b c)))
+     (square (min a b c))))
