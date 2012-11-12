c2b8f1850cd0fe13b9869bdd52bf89adc73d61d1
Greg Sexton <gregsexton@gmail.com>
2012-11-12 22:10:22 +0000

    Initial commit
    Project setup. No exercises done yet.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject sicp "0.1.0"
+  :description "Exercise answers to SICP"
+  :url "http://www.gregsexton.org/"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ src/sicp/ch2.clj
@@ -0,0 +1,1 @@
+(ns sicp.ch2)
