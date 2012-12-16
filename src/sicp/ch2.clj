7147c221f1accee52ffa6e807bd18a8fefbdf7db
Greg Sexton <gregsexton@gmail.com>
2012-12-16 21:22:10 +0000

    Complete exercises up to exercise 2.56

--- src/sicp/ch2.clj
+++ src/sicp/ch2.clj
@@ -436,3 +436,76 @@
 (def get-edge-2 (comp second second))
 
 ;;; Exercise 2.48
+
+(defn make-segment [start end] [start end])
+(def start-segment first)
+(def end-segment second)
+
+;;; Exercise 2.49
+
+(segments->painter
+ (list
+  (make-segment (make-vect 0 0)
+                (make-vect 0 1))
+  (make-segment (make-vect 0 1)
+                (make-vect 1 1))
+  (make-segment (make-vect 1 1)
+                (make-vect 1 0))
+  (make-segment (make-vect 1 0)
+                (make-vect 0 0))))
+
+(segments->painter
+ (list
+  (make-segment (make-vect 0 0)
+                (make-vect 1 1))
+  (make-segment (make-vect 0 1)
+                (make-vect 1 0))))
+
+(segments->painter
+ (list
+  (make-segment (make-vect 0.5 0)
+                (make-vect 0 0.5))
+  (make-segment (make-vect 0 0.5)
+                (make-vect 0.5 1))
+  (make-segment (make-vect 0.5 1)
+                (make-vect 1 0.5))
+  (make-segment (make-vect 1 0.5)
+                (make-vect 0.5 0))))
+
+;;; Exercise 2.50
+
+(defn flip-horiz [painter]
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(defn rotate-180 [painter]
+  (flip-vert (flip-horiz painter)))
+
+;;; Exercise 2.51
+
+(defn below [painter1 painter2]
+  (let [split-point (make-vect 0.0 0.5)]
+    (let [paint-below
+          (transform-painter painter1
+                             (make-vect 0.0 0.0)
+                             (make-vect 1.0 0.0)
+                             split-point)
+          paint-above
+          (transform-painter painter2
+                             split-point
+                             (make-vect 1.0 0.5)
+                             (make-vect 0.0 1.0))]
+      (fn [frame]
+        (paint-below frame)
+        (paint-above frame)))))
+
+(defn below [p1 p2]
+  (rotate-90
+   (beside
+    (rotate-270 p1)
+    (rotate-270 p2))))
+
+;;; Exercise 2.56
+
