3b19e883491c8f3eaaf7926144ceeafbca3a4f04
Greg Sexton <gregsexton@gmail.com>
2013-01-26 11:33:45 +0000

    Complete exercises up to 3.28

--- src/sicp/ch3.clj
+++ src/sicp/ch3.clj
@@ -46,7 +46,7 @@
                  :else (call x)))
       ([x & xs] (apply call x xs)))))
 
-;;; Exercise 3.3
+;;; Exercise 3.3/4
 
 (defn make-account [balance password]
   (let [balance (atom balance)
@@ -72,4 +72,102 @@
               (throw (RuntimeException. "Password incorrect."))))))
     dispatch))
 
-;;; Exercise 3.4
+;;; Exercise 3.5
+
+(defn monte-carlo [trials experiment]
+  (loop [remaining trials
+         passed 0]
+    (cond (= remaining 0) (/ passed trials)
+          (experiment) (recur (dec remaining) (inc passed))
+          :else (recur (dec remaining) passed))))
+
+(defn rand-in-range [low high]
+  (+ low (* (Math/random) (- high low))))
+
+(defn estimate-integral [P x1 x2 y1 y2 trials]
+  (let [area (* (- x2 x1) (- y2 y1))]
+    (* area
+       (monte-carlo trials #(P (rand-in-range x1 x2)
+                               (rand-in-range y1 y2))))))
+
+;;; Exercise 3.7
+
+(defn make-joint [acct pass new-pass]
+  (fn [p m]
+    (if (= p new-pass)
+      (acct pass m)
+      (throw (RuntimeException. "Password incorrect.")))))
+
+;;; Exercise 3.24
+
+(defn make-table []
+  (let [local-table (atom {})]
+    (defn lookup [k1 k2]
+      (let [subtable (get @local-table k1)]
+        (when subtable
+          (get subtable k2))))
+    (defn insert! [k1 k2 val]
+      (let [subtable (get @local-table k1 {})]
+        (swap! local-table
+               assoc k1 (assoc subtable k2 val))))
+    (def dispatch-table {:lookup lookup
+                         :insert insert!})
+    #(% dispatch-table)))
+
+(deftype Alist [eq? contents]
+  clojure.lang.IPersistentMap
+  (assoc [_ k v]
+    (Alist. eq? (cons [k v] contents)))
+  (assocEx [self k v]
+    (.assoc self k v))
+  (without [self k]
+    (Alist. eq? (filter (not #(= (first %) k))
+                    contents)))
+
+  java.lang.Iterable
+  (iterator [_]
+    (let [contents (atom contents)]
+      (proxy [java.util.Iterator] []
+        (hasNext [] (not (nil? @contents)))
+        (next []
+          (let [entry (first @contents)]
+            (swap! contents next)
+            (clojure.lang.MapEntry. (first entry)
+                                    (second entry)))))))
+
+  clojure.lang.Associative
+  (containsKey [self k]
+    (not (nil? (.entryAt self k))))
+  (entryAt [_ k]
+    (let [entry (some #(when (eq? (first %) k) %) contents)]
+      (when entry
+        (clojure.lang.MapEntry. (first entry)
+                                (second entry)))))
+
+  clojure.lang.IPersistentCollection
+  (count [_] (count contents))
+  (cons [_ o] (cons o contents))
+  (empty [_] (Alist. eq? '()))
+  (equiv [_ o]
+    (and (isa? (class o) Alist) (= o contents)))
+
+  clojure.lang.Seqable
+  (seq [self] (iterator-seq (.iterator self)))
+
+  clojure.lang.ILookup
+  (valAt [self k] (.valAt self k nil))
+  (valAt [self k not-found]
+    (let [entry (.entryAt self k)]
+      (if entry
+        (.val entry)
+        not-found))))
+
+(defn alist-map
+  ([] (Alist. = '()))
+  ([k v & keyvals]
+     (assoc (apply alist-map keyvals)
+       k v)))
+
+;;; Exercise 3.28
+
+
